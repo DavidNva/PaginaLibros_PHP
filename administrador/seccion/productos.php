1123bb31a91c4b1436ebb2cb1f08258078d4c221
David Nava <david.nava.garcia4@gmail.com>
2023-05-08 22:56:24 -0600

    Secciones de administrador, login, template, productos, diseño de acciones y tabla para mostrar datos

---
+++ administrador/seccion/productos.php
@@ -0,0 +1,63 @@
+<?php include("../template/cabecera.php"); ?> <!-- Hace referencia a la cabecera de administrador -->
+
+<div class="col-md-5">
+<div class="card">
+    <div class="card-header">
+        Datos de Libro
+    </div>
+
+    <div class="card-body">
+        <form method="$_POST" enctype="multipart/form-data">
+            <div class = "form-group">
+                <label for="txtID">ID:</label>
+                <input type="text" class="form-control" name="txtID" id="txtID" placeholder="ID">
+            </div>
+
+            <div class = "form-group">
+                <label for="txtNombre">Nombre:</label>
+                <input type="text" class="form-control" name="txtNombre" id="txtNombre" placeholder="Nombre del libro">
+            </div>
+            
+            <div class = "form-group">
+                <label for="txtNombre">Imagen:</label>
+                <input type="file" class="form-control" name="txtNombre" id="txtNombre" placeholder="Nombre del libro">
+            </div>
+
+
+            <div class="btn-group" role="group" aria-label="">
+                <button type="button" class="btn btn-success">Agregar</button>
+                <button type="button" class="btn btn-warning">Modificar</button>
+                <button type="button" class="btn btn-info">Cancelar</button>
+            </div>
+        </form>
+    
+    </div>
+</div>
+
+    
+    
+</div>
+
+<div class="col-md-7">
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Imagen</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>2</td>
+                <td>Aprende php</td>
+                <td>Imagen.jpg</td>
+                <td>Seleccionar | Borrar </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+
+<?php include("../template/pie.php"); ?> <!-- Hace referencia al pie de cabecera de administrador -->
